a90c8d6644960b99af9efab33f984b4201551521
CCET <you@example.com>
2019-11-04 11:17:14 +0100

    Initial commit

---
+++ src/be/qdp/Main.java
@@ -0,0 +1,20 @@
+package be.qdp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	char myChar = 'D';
+	char myUnicodeChar = '\u0044';
+	System.out.println(myChar);
+	System.out.println(myUnicodeChar);
+	char myCopyrightChar = '\u00A9';
+	System.out.println(myCopyrightChar);// write your code here;
+
+	boolean myTrueBoolenValue = true;
+	boolean myFalseBoolenValue = false;
+
+	boolean isCostumerOverTwentyOne = true;// write your code here
+
+    }
+}
